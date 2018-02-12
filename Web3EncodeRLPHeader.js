8c7734291e9309d22a936bdea204a485c719292c
Chance Santana-Wees <figs999@gmail.com>
2018-02-12 14:56:16 -0800

    Fixed problem with RLP encoding script.

--- Web3EncodeRLPHeader.js
+++ Web3EncodeRLPHeader.js
@@ -9,32 +9,43 @@ Note: This is a very simple RLP encoder, it won't encode anything but a block he
 */
 
 var encodeRLPHeader = function(block) {
-	return  hexToBytes("f90205" + 
-				encode_with_length(block.parentHash.slice(2)) +
-				encode_with_length(block.sha3Uncles.slice(2)) +
-				encode_with_length(block.miner.slice(2)) +
-				encode_with_length(block.stateRoot.slice(2)) +
-				encode_with_length(block.transactionsRoot.slice(2)) +
-				encode_with_length(block.receiptsRoot.slice(2)) +
-				"b90100" + block.logsBloom.slice(2) + 
-				encode_with_length(block.difficulty.toString(16)) +
-				encode_with_length(block.number.toString(16)) + 
-				encode_with_length(block.gasLimit.toString(16)) + 
-				encode_with_length(block.gasUsed.toString(16)) + 
-				encode_with_length(block.timestamp.toString(16)) + 
-				encode_with_length(block.extraData.slice(2)) +
-				encode_with_length(block.mixHash.slice(2)) + 
-				encode_with_length(block.nonce.slice(2)))
+	var output = 
+		encode_with_length(block.parentHash.slice(2),128) +
+		encode_with_length(block.sha3Uncles.slice(2),128) +
+		encode_with_length(block.miner.slice(2),128) +
+		encode_with_length(block.stateRoot.slice(2),128) +
+		encode_with_length(block.transactionsRoot.slice(2),128) +
+		encode_with_length(block.receiptsRoot.slice(2),128) +
+		encode_with_length(block.logsBloom.slice(2),128) + 
+		encode_with_length(block.difficulty.toString(16),128) +
+		encode_with_length(block.number.toString(16),128) + 
+		encode_with_length(block.gasLimit.toString(16),128) + 
+		encode_with_length(block.gasUsed.toString(16),128) + 
+		encode_with_length(block.timestamp.toString(16),128) + 
+		encode_with_length(block.extraData.slice(2),128) +
+		encode_with_length(block.mixHash.slice(2),128) + 
+		encode_with_length(block.nonce.slice(2),128);
+	return encode_length(output, 192) + output;
+}
+
+var encode_length = function(input, offset) {
+	L = input.length/2;
+	if(L < 56) {
+		return (L + offset).toString(16);
+	} else {
+		encode_with_length
+		return encode_with_length(L.toString(16),55+offset);
+	}
+}
+
+var encode_with_length = function(input, offset) {
+	if(input.length % 2 != 0)
+		input = "0" + input;
+	return encode_length(input, offset) + input;
 }
 
 function hexToBytes(hex) {
 	for (var bytes = [], c = 0; c < hex.length; c += 2)
 	bytes.push(parseInt(hex.substr(c, 2), 16));
 	return bytes;
-}
-
-var encode_with_length = function(input) {
-	if(input.length % 2 != 0)
-		input = "0" + input;
-	return (input.length/2 + 128).toString(16) + input;
 }
